210f4e53b644415d61590b3a7a2d18937d20f062
manana <palcgut2303@g.educaand.es>
2023-11-22 12:50:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tarea3_1/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.tarea3_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        menu.setGroupVisible(R.id.grupoMenu,true);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id=item.getItemId();
+        if(id == R.id.preferencias){
+            Intent intent = new Intent(this,SettingsActivity.class);
+            startActivity(intent);
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
