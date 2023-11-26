bc1218ec97fde2eac3374977d355550cc85aacb6
PabloAndroidStudio <palcgut2303@g.educaand.es>
2023-11-26 22:36:31 +0100

    Commit 22:36

--- app/src/main/java/com/example/tarea3_1/MainActivity.java
+++ app/src/main/java/com/example/tarea3_1/MainActivity.java
@@ -1,19 +1,36 @@
 package com.example.tarea3_1;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceManager;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_modoJuego;
+TextView tv_baseDatos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_modoJuego = findViewById(R.id.dificultad2);
+        tv_baseDatos = findViewById(R.id.textView4);
     }
 
     @Override
@@ -29,10 +46,34 @@ public class MainActivity extends AppCompatActivity {
         int id=item.getItemId();
         if(id == R.id.preferencias){
             Intent intent = new Intent(this,SettingsActivity.class);
-            startActivity(intent);
+            lanzador.launch(intent);
+
+
         }
 
 
         return super.onOptionsItemSelected(item);
     }
+
+    ActivityResultContract<Intent, ActivityResult> contrato = new ActivityResultContracts.StartActivityForResult();
+    ActivityResultCallback<ActivityResult> respuesta = new ActivityResultCallback<ActivityResult>(){
+        @SuppressLint("SetTextI18n")
+        @Override
+        public void onActivityResult(ActivityResult o) {
+            if (o.getResultCode() == Activity.RESULT_OK) {
+                //No hay códigos de actividad
+                Intent intentDevuelto = o.getData();
+                String modo = (String) intentDevuelto.getExtras().get("Modo");
+                String tipoBaseDatos = (String) intentDevuelto.getExtras().get("tipoBaseDatos");
+                String nombreServidor = (String) intentDevuelto.getExtras().get("nombreServidor");
+                String ipServidor = (String) intentDevuelto.getExtras().get("ipServidor");
+
+                tv_modoJuego.setText(modo);
+                tv_baseDatos.setText(tipoBaseDatos + " " + nombreServidor + " " + ipServidor);
+            }
+        }
+    };
+
+    ActivityResultLauncher<Intent> lanzador = registerForActivityResult(contrato,respuesta);
+
 }
--- app/src/main/java/com/example/tarea3_1/SettingsActivity.java
+++ app/src/main/java/com/example/tarea3_1/SettingsActivity.java
@@ -1,5 +1,8 @@
 package com.example.tarea3_1;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -7,9 +10,15 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    String modoSeleccionado = "";
+    String tipoBaseDatos = "";
+    String nombreServidor = "";
+    String ipServidor = "";
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +33,10 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+
+
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -38,6 +51,27 @@ public class SettingsActivity extends AppCompatActivity {
         // Si se selecciona el botón atrás de la barra de menú,
         // cerramos la actividad Preferencias
         if (item.getItemId() == android.R.id.home) {
+            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+            modoSeleccionado = sharedPreferences.getString("categoria","Medio");
+            tipoBaseDatos = String.valueOf(sharedPreferences.getBoolean("tipoBaseDatos",false));
+            if(tipoBaseDatos.equalsIgnoreCase("true")){
+                tipoBaseDatos = "Externa: ";
+                nombreServidor = sharedPreferences.getString("nombre","Nombre de la base de datos");
+                ipServidor = sharedPreferences.getString("ip","IP del servidor");
+            }else{
+                tipoBaseDatos = "Intera SQLite";
+                nombreServidor = "";
+                ipServidor = "";
+            }
+
+
+            Intent intentVolver = new Intent();
+            intentVolver.putExtra("Modo",modoSeleccionado);
+            intentVolver.putExtra("tipoBaseDatos",tipoBaseDatos);
+            intentVolver.putExtra("ipServidor",ipServidor);
+            intentVolver.putExtra("nombreServidor",nombreServidor);
+            setResult(RESULT_OK, intentVolver);
             finish();
             return true;
         }
